52f4c1e1bf1e47fa813b4f8c692571d150643c5d
YAMASAKI Masahide <masahide.y@gmail.com>
2019-08-04 22:00:33 +0900

    fix

--- update_repo.sh
+++ update_repo.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-echo '```' >> log
+curl -sS -X POST -H 'Content-type: application/json' --data '{"text":"start createrepo"}' "$SLACK_URL"
 set -x
-aws s3 sync --quiet --exclude "*" --include repodata --include RPMS "$S3SYNC_FROM" repo 2>&1 |tee -a log
-createrepo --update ./repo  2>&1 |tee -a log
-find repo/RPMS -type f -mtime -10 -ls |tee -a log
-aws s3 sync --no-progress --delete repo/repodata "$S3SYNC_FROM/repodata"  2>&1 |tee -a log
-echo '```' >> log
+mkdir -p ./repo
+aws s3 sync --no-progress "$S3SYNC_FROM/repodata" ./repo/repodata
+aws s3 sync --no-progress "$S3SYNC_FROM/RPMS" ./repo/RPMS
+(
+set -x
+find repo/RPMS -type f -mtime -10 -ls
+createrepo --update ./repo
+aws s3 sync --no-progress --delete repo/repodata "$S3SYNC_FROM/repodata"
+) 2>&1 |tee -a log
 aws cloudfront create-invalidation --distribution-id "$CF_DISTRIID" --paths "$CF_PATH"
-
-curl -XPOST -H 'Content-type: application/json' --data "{\"text\": $(cat log|python -c 'import json,sys; print(json.dumps(sys.stdin.read()))')}" "$SLACK_URL"
+cat << EOS > data
+end createrepo
+\`\`\`
+$(cat log)
+\`\`\`
+EOS
+cat << EOS > log
+{ "text": $(cat data|python -c 'import json,sys; print(json.dumps(sys.stdin.read()))') }
+EOS
+curl -XPOST -H 'Content-type: application/json' -d @log "$SLACK_URL"
+rm -rf log data
